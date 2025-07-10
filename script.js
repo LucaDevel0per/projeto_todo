ff749c2dd02ed9de0e8a828f3df319f8d97a84b5
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-10 18:27:12 -0300

    feat: Melhorando função

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ const form = document.getElementById('todo-form')
 const input = document.getElementById('todo-input')
 const list = document.getElementById('todo-list')
 
+let tarefas = []
+
+function renderizarTarefas() {
+    list.innerHTML = '';
+
+    tarefas.forEach((tarefa, idx) => {
+        const li = document.createElement('li')
+        li.textContent = tarefa;
+
+        list.appendChild(li)
+    })
+}
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     //
     const tarefa = input.value;
 
     if (tarefa.trim() !== '') {
-        list.innerHTML += `<li>${tarefa}</li>`
-        input.value = '';
+        // list.innerHTML += `<li>${tarefa}</li>`
+        // input.value = '';
+        tarefas.push(tarefa);
+    input.value = '';
+    renderizarTarefas();
+
     } else {
         alert("Por favor, digite uma tarefa válida!");
     }
